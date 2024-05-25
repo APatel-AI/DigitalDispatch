01eb6014797cf64fd85bc95e297ef3cc10915098
APatel-AI <arpan.p.se@gmail.com>
2024-05-25 17:12:49 -0500

    create user model and schema, set up mongodb database api to store registered user data

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import '../App.css'
+import React, { useState } from "react";
+import "../App.css";
+
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function registerUser(ev){
+    ev.preventDefault();
+    await fetch('http://localhost:4000/register', {
+      method: 'POST', 
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type':'application/json'}
+    })
+  }
   return (
-    <form className='register'>
-    <h1>Register</h1>
-    <input type='text' placeholder='username'/>
-    <input type='password' placeholder='password'/>
-    <button>Register</button>
+    <form className="register" onSubmit={registerUser}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={ev => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={ev => setPassword(ev.target.value)}
+      />
+      <button>Register</button>
+    </form>
+  );
+};
 
-</form>
-  )
-}
-
-export default Register
+export default Register;
