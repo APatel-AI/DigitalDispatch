526baee8378430dc6b8ddfb25a25b46dfafbf69d
APatel-AI <arpan.p.se@gmail.com>
2024-05-25 18:16:15 -0500

    integrated toast api for error handling, secured the api keys, created test template for backend connection testing

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,45 @@ import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import Register from "./components/Register";
-
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
+    <>
+    <Toaster
+          position="bottom-center"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            // Define default options
+            className: "",
+            duration: 5000,
+            style: {
+              background: "#363636",
+              color: "#fff",
+            },
+
+            // Default options for specific types
+            success: {
+              duration: 3000,
+              theme: {
+                primary: "green",
+                secondary: "black",
+              },
+            },
+          }}
+        />
+    
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home/>} />
-        <Route path={"/login"} element={<LoginPage/>} />
-        <Route path={"/register"} element={<Register/>} />
+        <Route index element={<Home />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/register"} element={<Register />} />
+        
       </Route>
     </Routes>
+    </>
   );
 }
 
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import "../App.css";
+import toast, {Toaster} from "react-hot-toast";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  async function registerUser(ev){
+  async function registerUser(ev) {
     ev.preventDefault();
-    await fetch('http://localhost:4000/register', {
-      method: 'POST', 
-      body: JSON.stringify({username, password}),
-      headers: {'Content-Type':'application/json'}
-    })
+
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.status === 200) {
+      toast.success('Successfully created!');
+
+    } else {
+      toast.error('This is an error!');
+
+    }
   }
   return (
     <form className="register" onSubmit={registerUser}>
@@ -19,13 +29,13 @@ const Register = () => {
         type="text"
         placeholder="username"
         value={username}
-        onChange={ev => setUsername(ev.target.value)}
+        onChange={(ev) => setUsername(ev.target.value)}
       />
       <input
         type="password"
         placeholder="password"
         value={password}
-        onChange={ev => setPassword(ev.target.value)}
+        onChange={(ev) => setPassword(ev.target.value)}
       />
       <button>Register</button>
     </form>
