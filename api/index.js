09793b7d00a34a125a11b5a09c2d5ad72de47c99
APatel-AI <arpan.p.se@gmail.com>
2024-05-27 17:16:11 -0500

    created secured route for registering users, user registration succesfully loads onto mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/User");
+const dotenv = require("dotenv/config");
+const bcrypt = require("bcryptjs");
 
-app.get('/test', (req,res) => {
-    res.json('test ok')
-})
+const salt = bcrypt.genSaltSync(10);
+
+
+mongoose.connect(process.env.MONGO_URL);
+console.log(process.env.MONGO_URL);
+app.use(cors());
+
+app.use(express.json());
+
+app.post('/register', async (req,res) => {
+  const {username,password} = req.body;
+  try{
+    const userDoc = await User.create({
+      username,
+      password:bcrypt.hashSync(password,salt),
+    });
+    res.json(userDoc);
+  } catch(e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
 app.listen(4000);
