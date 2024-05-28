0489dfd674db818bcdc414cafd4b65ce6273c664
APatel-AI <arpan.p.se@gmail.com>
2024-05-28 12:29:34 -0500

    updated endpoint url for deployment

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -8,7 +8,7 @@ const Register = () => {
   async function registerUser(ev) {
     ev.preventDefault();
 
-    const response = await fetch("http://localhost:4000/register", {
+    const response = await fetch("https://digital-dispatch-8bo6cnxnl-apatelais-projects.vercel.app", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
