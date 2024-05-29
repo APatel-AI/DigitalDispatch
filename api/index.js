738c835fe746b7b5e41c5a5af82fa76ecdebf00f
APatel-AI <arpan.p.se@gmail.com>
2024-05-29 05:27:02 -0500

    configured terminal deployment through vercel

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ console.log(process.env.MONGO_URL);
 app.use(
   cors({
     credentials: true,
-    origin: "https://digital-dispatch.vercel.app",
+    origin: "https://digital-dispatch.vercel.app/",
     
   })
 );
